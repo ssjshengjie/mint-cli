2aeeaa80ce2f49731406581749e90f46d65bcc9b
sunshengjie <sunshengjie@joygames.cc>
2022-09-19 09:56:44 +0800

    docs: readme modify

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# a easy cli of mint
+# a simple and fast way to build the cli of the vue3 project
+
+<p align="center">
+  <img src="https://github.com/ssjshengjie/mint-cli/lib/img/bk.png" width="800">
+</p>
+
+## Usage
+
+```sh
+npm intall minto-cli
+```
--- lib/core/help.ts
+++ lib/core/help.ts
@@ -3,5 +3,6 @@ import { program } from "commander";
 export const helpOptions = () => {
   program
     .option("-d --dest <dest>", "a destination folder")
-    .option("-f --frame <frame>", "Choose the framework");
+    .option("-f --frame <frame>", "Choose the framework")
+    .option("-c --com <components>", "create project components");
 };
